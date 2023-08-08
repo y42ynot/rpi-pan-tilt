1c49723b75490362b2e2c346dc33dd12ca950dda
y42ynot <135897452+y42ynot@users.noreply.github.com>
2023-08-08 12:22:23 -0600

    did stuff

--- run.py
+++ run.py
@@ -5,24 +5,24 @@
 from sshkeyboard import listen_keyboard
 
 # variables
-panpins = [1,2,3,4] # pin 1-4 on the stepper motor on pi
-tiltpins = [1,2,3,4]
-tiltlimitpin = 10
-panlimitpin = 10
+panpins = [8,10,12,16] # pin 1-4 on the stepper motor on pi
+tiltpins = [7,11,13,15]
+tiltlimitpin = 21
+panlimitpin = 19
 panpos = 0
 tiltpos = 0
-panlim = 1000
-tiltlim = 1000
+panlim = 11410
+tiltlim = 12190
 
 
 # functions
 def pan(steps,direction): #true is clockwise false is counterclockwise                                   
     #RpiMotorLib.BYJMotor("MyMotorOne", "28BYJ").motor_run(panpins,.001, steps, direction, False, "half", .05)
-    print("pan equals "+str(panpos))
+    print("pan equals "+str(round((panpos/panlim)*180,1))+" degrees")
 
 def tilt(steps,direction):
     #RpiMotorLib.BYJMotor("MyMotorOne", "28BYJ").motor_run(tiltpins,.001, steps, direction, False, "half", .05)
-    print("tilt equals "+str(tiltpos))
+    print("tilt equals "+str(round((tiltpos/tiltlim)*180,1))+" degrees")
 
 def checkpan(steps,direction):
     global panpos
@@ -38,7 +38,7 @@ def checkpan(steps,direction):
             print("sorry, but that would push me past my limits. please try moving the other way")
             return False
         else:
-            panpos = panpos + steps
+            panpos = panpos - steps
             return True
 def checktilt(steps,direction):
     global tiltpos
@@ -50,71 +50,79 @@ def checktilt(steps,direction):
             tiltpos = tiltpos + steps
             return True
     else:
-        if tiltpos - steps < 0:
+        if tiltpos - steps < 10:
             print("sorry, but that would push me past my limits. please try moving the other way")
             return False
         else:
             tiltpos = tiltpos - steps
             return True
 
- 
-# def init():
-#     print("gimme a moment to set stuff up so you don't screw anything up")
-    #GPIO.setwarnings(False) # Ignore warning for now #sets up limit switches
-    #GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-    #GPIO.setup(tiltlimitpin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    #GPIO.setwarnings(False) # Ignore warning for now #sets up limit switches
-    #GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-    #GPIO.setup(panlimitpin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  
+def init():
+    global panpos
+    global tiltpos
+    print("gimme a moment to set stuff up so you don't screw anything up")
+    GPIO.setwarnings(False) # Ignore warning for now #sets up limit switches
+    GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+    GPIO.setup(tiltlimitpin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setwarnings(False) # Ignore warning for now #sets up limit switches
+    GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+    GPIO.setup(panlimitpin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  
 
 
     #zero pan
-    # while True:
-    #     if GPIO.input(panlimitpin) == GPIO.HIGH:
-    #         pan(1,True) 
-
-    #     else:
-    #         break
-    # #zero tilt
-    # while True:
-    #     if GPIO.input(tiltlimitpin) == GPIO.HIGH:
-    #         tilt(1,True) 
+    while True:
+        if GPIO.input(panlimitpin) == GPIO.HIGH: 
+            panpos = 5978
+            break
+        else:
+            pan(1,True)
+    #zero tilt
+    while True:
+        if GPIO.input(tiltlimitpin) == GPIO.HIGH:
+            tilt(600,False)
+            tiltpos = 0
+            break
+        else:
+            tilt(1,True) 
 
 
 # logs keystrokes assigns roles to them
 def press(key):
     if key == "up":
-        if checktilt(20,True):
-            tilt(20,True)
+        if checktilt(500,True):
+            tilt(500,True)
 
     elif key == "down":
-        if checktilt(20,False):
-            tilt(20,False)
+        if checktilt(500,False):
+            tilt(500,False)
  
         print("")
     elif key == "left":
-        if checkpan(20,True):
-            pan(20,True)
+        if checkpan(500,True):
+            pan(500,True)
 
     elif key == "right":
-        if checkpan(20,False):
-            pan(20,False)
+        if checkpan(500,False):
+            pan(500,False)
 
     elif key == "w":
-        if checktilt(1,True):
-            tilt(1,True)
+        if checktilt(100,True):
+            tilt(100,True)
  
     elif key == "s":
-        if checktilt(1,False):
-            tilt(1,False)
+        if checktilt(100,False):
+            tilt(100,False)
 
     elif key == "a":
-        if checkpan(1,True):
-            pan(1,True)
+        if checkpan(100,True):
+            pan(100,True)
 
     elif key == "d":
-        if checkpan(1,False):
-            pan(1,False)
+        if checkpan(100,False):
+            pan(100,False)
+
+def end():
+    pass
         
 
 
